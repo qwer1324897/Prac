45303719fbfcb48f3e73abd659cd727cef0bbde1
qwer1324897 <qwer1324897@gmail.com>
2025-10-23 02:03:05 +0900

    1023

--- stm/src/service/Service.java
+++ stm/src/service/Service.java
@@ -4,6 +4,11 @@ public class Service {
 
 
     public void addStudent() {
+        IoManager.print("====== 학생 정보 등록 ======");
+        IoManager.printAndInputString("\n등록하실 학생의 이름을 입력해 주세요.\n이름 : ");
+        IoManager.printAndInputInteger("\n등록하실 학생의 나이를 입력해 주세요.\n나이 : ");
+        IoManager.printAndInputInteger("\n등록하실 학생의 성적을 입력해 주세요.\n성적 : ");
+        IoManager.print("\"" + name + "\"" + "")
 
     }
 
